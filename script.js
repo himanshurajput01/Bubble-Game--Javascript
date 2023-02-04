5e9fba86d06dcc96268bdd3f9eaa6662af7c8abe
Himanshu Singh Rajput <106831924+himanshurajput01@users.noreply.github.com>
2023-02-04 23:28:25 +0530

    html,css,js project

---
+++ script.js
@@ -0,0 +1,50 @@
+var timer = 60;
+var score = 0;
+var timerelem = document.querySelector('#timerelem');
+var hitelem = document.querySelector('#hitelem');
+var scoreelem = document.querySelector('#scoreelem');
+var btmpnl = document.querySelector('#btmpnl');
+
+var guessNumber;
+
+
+setInterval(function(){
+    if(timer > 0){
+        --timer;
+        timerelem.textContent = timer;
+    }
+}, 1000);
+
+function makeBubbles(){
+    for(var i= 0; i<171;i++){
+    var randomNumber = Math.floor(Math.random()*10);
+    var template = `<div class="bubble">${randomNumber}</div>`;
+    btmpnl.innerHTML += template;
+}
+}
+
+
+function getNewGuess() {
+    //getting a random Number
+     guessNumber = Math.floor(Math.random()*10);
+    //and then place the random number inside the game guess element
+    hitelem.textContent = guessNumber;
+}
+
+btmpnl.addEventListener('click', function(details){
+    var clickedBblValue = details.target.textContent;
+    if(clickedBblValue == guessNumber) {
+        score = score + 10;
+        scoreelem.textContent = score;
+        getNewGuess();
+        btmpnl.innerHTML = '';
+        makeBubbles();
+    }
+    else {
+        btmpnl.innerHTML = '<h1 id"center" >Game Over</h1>'
+    }
+})
+
+
+getNewGuess();
+makeBubbles();
